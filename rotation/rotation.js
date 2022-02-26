bcd39e239066543a3a8b19bd29963f4b19ce00f9
hediwei <1310742186@qq.com>
2022-02-27 00:26:02 +0800

    更改浮动样式

--- rotation/rotation.js
+++ rotation/rotation.js
@@ -13,3 +13,12 @@ setInterval(function () {
     //将下一个元素移到中间
     next.setAttribute('data-show', '')
   }, 2600)
+
+// 1.mask为啥设置height为106px？
+// 刚好为一组轮播字的高度；
+// 2.span为何这样设置css？
+// 为了轮播字都重叠在一起；后往下移动（top:100px),因为mask设置了overflow:hidden，所以看不见
+// 3.为何单独设置data-show属性?
+// 这是为了让具备该属性的span向上移动一倍高度的距离；
+// 4.什么是background-clip?
+// 这是用文字作为遮色片，结合text-fill-color:transparent和background-repeat:no repeat,将各需要轮播的文字设置对应的渐层文字，即可实现渐变效果。
