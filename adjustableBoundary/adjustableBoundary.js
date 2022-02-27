bb49d9044fb7af570892bb577d5d951e6098cf86
hediwei <1310742186@qq.com>
2022-02-28 00:05:33 +0800

    更改显示名称

--- adjustableBoundary/adjustableBoundary.js
+++ adjustableBoundary/adjustableBoundary.js
@@ -1,4 +1,5 @@
 var startX, startWidth
+//为了使得刷新后分隔栏的位置仍然不变
 startWidth = localStorage.getItem('scalable_width') || getScalableDivWidth()
 
 document.querySelector('.scalable').style.width = startWidth + 'px'
@@ -6,14 +7,17 @@ document.querySelector('.scalable').style.width = startWidth + 'px'
 document.querySelector('.separator').addEventListener('mousedown', startDrag)
 
 function startDrag(e) {
+  //startX用来记录按下鼠标的那一刻，鼠标的X坐标
   startX = e.clientX
+  //startWidth用来记录按下鼠标的那一刻，左边scalable的宽度
   startWidth = getScalableDivWidth()
   
   document.documentElement.addEventListener('mousemove', onDrag)
   document.documentElement.addEventListener('mouseup', stopDrag)
 }
-
+//e指的是事件发生后，传递回来的事件资料
 function onDrag(e) {
+    //e.clientX - startX可得到鼠标移动的距离 newWidth得到的是新的宽度
   let newWidth = startWidth + e.clientX - startX
   document.querySelector('.scalable').style.width = newWidth + 'px'
 }
@@ -25,5 +29,6 @@ function stopDrag(e) {
 }
 
 function getScalableDivWidth() {
+  //10为进制 得到的结果不带单位
   return parseInt(window.getComputedStyle(document.querySelector('.scalable')).width, 10)
 }
